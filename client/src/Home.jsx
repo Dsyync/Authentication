662eb805efe6bd900f2091b6a663f21827647f0f
Dsyync <mdtousif8410@gmail.com>
2024-08-22 13:03:13 +0530

    files upload

---
+++ client/src/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Home () {
+    return(
+        <div className="bg-white p-5 rounded w-50">
+            <h2>Welcome to Home page</h2>
+        </div>
+
+    )
+}
+
+export default Home;
